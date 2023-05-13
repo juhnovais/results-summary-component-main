b5fb61294a48ca5e2c4fba64a30b1f98a3d95d43
juhfalleirosnovais <60365178+juhfalleirosnovais@users.noreply.github.com>
2023-05-13 23:34:20 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,45 @@
+Results summary component solution
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the interface depending on their device's screen size
+- See hover and focus states for all interactive elements on the page
+
+### Screenshot
+
+![](./screenshot.jpg)
+
+### Links
+
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+- JavaScript
+
+## Author
+
+- Website - Juliana Novais (https://www.your-site.com)
+
+
---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+
+
+
+  $.getJSON("data.json", function (data) {
+    var items = [];
+    var itemClass = '';
+    $.each( data, function( key, val ) {
+        if(val.category === 'Reaction'){
+            itemClass = 'reaction';
+        } else if (val.category === 'Memory') {
+            itemClass = 'memory';
+        } else if (val.category === 'Verbal') {
+            itemClass = 'verbal';
+        } else {
+            itemClass = 'visual';
+        }
+        items.push( '<div class="' + itemClass + '"><p><span class="sumarySingle"><img src="' + val.icon + '"><span>' + val.category + '</span></span><span>' + val.score + ' / <span class="percent">100</span></span></p></div>');
+       
+        
+        
+      });
+
+      console.log(data);
+     
+      $( "<div/>", {"class": "summaryDetails", html: items.join( "" )
+      }).appendTo( ".resultSummarySummary div:first-of-type" );
+  });
+});
